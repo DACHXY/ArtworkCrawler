416351ba5058e3b2dd0e87d65156edc958131b71
DACHXY <danny10132024@gmail.com>
2023-06-02 01:58:23 +0800

    更改 Artworks.json 的位置

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ TEMP_HTML_FILE_PATH = "TEMP.html"
 BASEURL: str = "https://www.artsy.net"
 URL: str = BASEURL + "/collection/new-this-week"
 API_URL: str = "https://metaphysics-production.artsy.net/v2"
-STORE_FILE_NAME = "Artworks.json"
+STORE_FILE_NAME = "data/Artworks.json"
 
 # graph QL
 SEARCH_DETAIL_QL = open("graphql/detail.graphql").read()
